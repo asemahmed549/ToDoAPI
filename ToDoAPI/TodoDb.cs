4405dfef5a711742523d6c731d80bcfa1d784003
Asem Abdelghafar <aabdelghafar@EFG-Hermes.com>
2024-09-11 15:13:25 +0300

    Add Minimal crud APIs

--- ToDoAPI/TodoDb.cs
+++ ToDoAPI/TodoDb.cs
@@ -1,6 +1,15 @@
-﻿namespace ToDoAPI
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ToDoAPI
 {
-	public class TodoDb
+	public class TodoDb : DbContext
 	{
-	}
+        public TodoDb(DbContextOptions<TodoDb> options)
+            : base(options)
+        {
+ 
+        }
+
+        public DbSet<TodoItem> Todos { get; set; }
+    }
 }
